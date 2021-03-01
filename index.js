dbb639d889c7d7238c3ba5b686e8d9836dce22d6
Balaga Gayatri <balaga-gayatri@github.com>
2021-03-01 11:40:37 +0530

    added execution time

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
+  var start = Date.now();
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
@@ -10,6 +11,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  var end = Date.now();
+  const execTime = end-start;
 } catch (error) {
   core.setFailed(error.message);
 }
